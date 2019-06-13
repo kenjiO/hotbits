4e0fef06a32b627941a92736a2e3877c12a788f7
Kenji Okamoto <kenjio@users.noreply.github.com>
2019-06-13 15:41:36 -0700

    mock api calls when running tests

--- src/index.test.js
+++ src/index.test.js
@@ -1,31 +1,57 @@
+import nock from 'nock';
 import hotbits from './index';
 
+function setupNock() {
+  const randomBytes = Array.from(
+    { length: hotbits.DEFAULT_NUMBER_OF_RESULTS },
+    () => Math.floor(Math.random() * 256),
+  );
+  nock('http://www.fourmilab.ch')
+    .get(`/cgi-bin/Hotbits.api?nbytes=${hotbits.DEFAULT_NUMBER_OF_RESULTS}&fmt=json&apikey=&pseudo=pseudo`)
+    .reply(200, { data: randomBytes });
+}
+
 describe('hotbits', () => {
+  afterAll(nock.restore);
+  afterEach(nock.cleanAll);
+
   it('returns a promise', () => {
+    setupNock();
     expect(hotbits() instanceof Promise).toBe(true);
   });
 
-  it('resolves to an array', () =>
-    hotbits().then((data) => {
+  it('resolves to an array', () => {
+    setupNock();
+    return hotbits().then((data) => {
       expect(Array.isArray(data)).toBe(true);
-    }));
+    });
+  });
 
-  it('array contains 10 results', () =>
-    hotbits().then((data) => {
+  it('array contains 10 results', () => {
+    setupNock();
+    return hotbits().then((data) => {
       expect(data.length).toBe(hotbits.DEFAULT_NUMBER_OF_RESULTS);
-    }));
+    });
+  });
 
-  it('each result is an 8 bit integer', () =>
-    hotbits().then((data) => {
+  it('each result is an 8 bit integer', () => {
+    setupNock();
+    return hotbits().then((data) => {
       data.forEach((number) => {
         expect(Number.isInteger(number)).toBe(true);
         expect(number).toBeGreaterThanOrEqual(0);
         expect(number).toBeLessThanOrEqual(255);
       });
-    }));
+    });
+  });
 
-  it('does not produce the same results', () =>
-    hotbits().then(data1 => hotbits().then((data2) => {
-      expect(data1).not.toEqual(data2);
-    })));
+  it('does not produce the same results', () => {
+    setupNock();
+    return hotbits().then((data1) => {
+      setupNock();
+      return hotbits().then((data2) => {
+        expect(data1).not.toEqual(data2);
+      });
+    });
+  });
 });
