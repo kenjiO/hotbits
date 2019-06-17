cfe01c01ba848359554af5b7078371978ad933ea
Kenji Okamoto <kenjio@users.noreply.github.com>
2019-06-17 09:12:10 -0700

    use api key

--- src/index.js
+++ src/index.js
@@ -2,15 +2,31 @@ import httpsRequest from './https-request';
 
 const DEFAULT_NUMBER_OF_RESULTS = 10;
 
-const hotbits = () => {
+const hotbits = (key) => {
+  if (key === undefined || key === '') {
+    return Promise.reject(new Error('No API key specified'));
+  }
+  if (key === null) {
+    return Promise.reject(new Error('API key must be type string. Got null.'));
+  }
+  if (typeof key !== 'string') {
+    return Promise.reject(new Error(`API key must be type string. Got ${typeof key}.`));
+  }
+
+  const encodedKey = encodeURIComponent(key);
+
   const params = {
     host: 'www.fourmilab.ch',
-    path: `/cgi-bin/Hotbits.api?nbytes=${DEFAULT_NUMBER_OF_RESULTS}&fmt=json&apikey=&pseudo=pseudo`,
+    path: `/cgi-bin/Hotbits.api?nbytes=${DEFAULT_NUMBER_OF_RESULTS}&fmt=json&apikey=${encodedKey}`,
   };
+
   return httpsRequest(params).then((res) => {
     try {
       return JSON.parse(res).data;
     } catch (e) {
+      if (res.includes('HotBits API Key invalid')) {
+        throw new Error('HotBits API Key invalid');
+      }
       throw new Error('Reply received from server not in expected JSON format');
     }
   });
--- src/index.test.js
+++ src/index.test.js
@@ -1,6 +1,8 @@
 import nock from 'nock';
 import hotbits from './index';
 
+const API_KEY = '1234567890abcdef';
+
 function randomBytes(quantity) {
   const length = (quantity === undefined) ? hotbits.DEFAULT_NUMBER_OF_RESULTS : quantity;
   return Array.from({ length }, () => Math.floor(Math.random() * 256));
@@ -8,7 +10,7 @@ function randomBytes(quantity) {
 
 function setupNock(statusCode, response) {
   nock('https://www.fourmilab.ch')
-    .get(`/cgi-bin/Hotbits.api?nbytes=${hotbits.DEFAULT_NUMBER_OF_RESULTS}&fmt=json&apikey=&pseudo=pseudo`)
+    .get(`/cgi-bin/Hotbits.api?nbytes=${hotbits.DEFAULT_NUMBER_OF_RESULTS}&fmt=json&apikey=${API_KEY}`)
     .reply(statusCode, response);
 }
 
@@ -18,26 +20,26 @@ describe('hotbits', () => {
 
   it('returns a promise', () => {
     setupNock(200, { data: randomBytes() });
-    expect(hotbits() instanceof Promise).toBe(true);
+    expect(hotbits(API_KEY) instanceof Promise).toBe(true);
   });
 
   it('resolves to an array', () => {
     setupNock(200, { data: randomBytes() });
-    return hotbits().then((data) => {
+    return hotbits(API_KEY).then((data) => {
       expect(Array.isArray(data)).toBe(true);
     });
   });
 
   it('array contains 10 results', () => {
     setupNock(200, { data: randomBytes() });
-    return hotbits().then((data) => {
+    return hotbits(API_KEY).then((data) => {
       expect(data.length).toBe(hotbits.DEFAULT_NUMBER_OF_RESULTS);
     });
   });
 
   it('each result is an 8 bit integer', () => {
     setupNock(200, { data: randomBytes() });
-    return hotbits().then((data) => {
+    return hotbits(API_KEY).then((data) => {
       data.forEach((number) => {
         expect(Number.isInteger(number)).toBe(true);
         expect(number).toBeGreaterThanOrEqual(0);
@@ -48,9 +50,9 @@ describe('hotbits', () => {
 
   it('does not produce the same results', () => {
     setupNock(200, { data: randomBytes() });
-    return hotbits().then((data1) => {
+    return hotbits(API_KEY).then((data1) => {
       setupNock(200, { data: randomBytes() });
-      return hotbits().then((data2) => {
+      return hotbits(API_KEY).then((data2) => {
         expect(data1).not.toEqual(data2);
       });
     });
@@ -58,18 +60,45 @@ describe('hotbits', () => {
 
   it('handles http error codes', () => {
     setupNock(404);
-    return expect(hotbits()).rejects.toThrow('http status code 404 received. Expected 200');
+    return expect(hotbits(API_KEY)).rejects.toThrow('http status code 404 received. Expected 200');
   });
 
   it('handles network errors', () => {
     nock('https://www.fourmilab.ch')
-      .get(`/cgi-bin/Hotbits.api?nbytes=${hotbits.DEFAULT_NUMBER_OF_RESULTS}&fmt=json&apikey=&pseudo=pseudo`)
+      .get(`/cgi-bin/Hotbits.api?nbytes=${hotbits.DEFAULT_NUMBER_OF_RESULTS}&fmt=json&apikey=${API_KEY}`)
       .replyWithError('getaddrinfo ENOTFOUND www.fourmilab.ch');
-    return expect(hotbits()).rejects.toThrow('getaddrinfo ENOTFOUND www.fourmilab.ch');
+    return expect(hotbits(API_KEY)).rejects.toThrow('getaddrinfo ENOTFOUND www.fourmilab.ch');
   });
 
-  it('throws errors for replies not in JSON format', () => {
+  it('throws an error for a reply not in JSON format', () => {
     setupNock(200, '1, 2, 3, 4');
-    return expect(hotbits()).rejects.toThrow('Reply received from server not in expected JSON format');
+    return expect(hotbits(API_KEY)).rejects.toThrow('Reply received from server not in expected JSON format');
+  });
+
+  it('throws an error for missing api key', () =>
+    expect(hotbits())
+      .rejects.toThrow('No API key specified'));
+
+  it('throws an error for number type api key', () =>
+    expect(hotbits(123456789))
+      .rejects.toThrow('API key must be type string. Got number'));
+
+  it('throws an error for object type api key', () =>
+    expect(hotbits({}))
+      .rejects.toThrow('API key must be type string. Got object.'));
+
+  it('throws an error for null type api key', () =>
+    expect(hotbits(null))
+      .rejects.toThrow('API key must be type string. Got null'));
+
+  it('throws an error for bool type api key', () =>
+    expect(hotbits(true))
+      .rejects.toThrow('API key must be type string. Got boolean'));
+
+  it('throws an error when server indicates invalid key', () => {
+    const response = '<!DOCTYPE html><html><head><title>HotBits</title></head>'
+      + '<body><b>HotBits API Key invalid.</b></body></html>';
+    setupNock(200, response);
+    return expect(hotbits(API_KEY)).rejects.toThrow('HotBits API Key invalid');
   });
 });
